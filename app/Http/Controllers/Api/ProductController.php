ff6195c350632fa5767b6bc70c2b0f6988ffc808
shahd_Elmnyar <shahdmohamed2632003@gmail.com>
2024-08-29 20:50:35 +0300

    ✨ Implement Product Controller for Enhanced Product Operations 🛍️
    📦 Created ProductController to manage product-related API endpoints.
    🔍 Added show method to display detailed product information including images, brand, additions, and rates.
    🔄 Introduced filter method to filter products by category and various criteria such as brand, order rate, and price.
    🔎 Implemented search method for searching products based on keywords with validation rules.
    📊 Enhanced API responses with pagination and resource formatting for a consistent output.

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Resources\ProductResource;
+use App\Http\Controllers\Api\MainController;
+
+class ProductController extends MainController
+{
+    private $rules ;
+    public function __construct(){
+        parent::__construct();
+        $this->rules = [
+            'search' => 'required|string|max:255',
+        ];
+    }
+
+    public function show( $id){
+        $product = Product::find($id);
+        if (!$product) {
+            return $this->errorResponse('home.product_not_found', 404 );
+        }
+        $product->load(['images', 'brand', 'additions','rates']);
+        $data = [new ProductResource($product),];
+        return $this->resourceResponse($data, 'home.home_success');
+    }
+
+    public function filter(Request $request, Category $category = null)
+    {
+        $filters = $request->only(['brand', 'top_ordered', 'order_by_rate', 'order_by_price']);
+
+        if ($category) {
+            $products = $category->products()->filter($filters)->paginate(4);
+        } else {
+
+            $products = Product::filter($filters)->paginate(4);
+        }
+
+        $data = ['products' =>ProductResource::collection($products)
+        , 'productsPagination' => $this->getPaginationData($products)];
+        return $this->resourceResponse($data, 'home.home_success');
+    }
+
+    function search(Request $request){
+        $validateRequest = $this->validateRequest($request, $this->rules);
+
+        if (isset($validateRequest)) {
+            return $validateRequest;
+        }
+        $filters = ['search' => $request->search];
+        $products = Product::filter($filters)
+            ->with(['images', 'brand', 'additions','rates'])
+            ->paginate(4);
+        $productResources = $products->isNotEmpty() ? ProductResource::collection($products) : [];
+        $data = ['products' => $productResources,
+                'productsPagination' => $this->getPaginationData($products)];
+
+        return $this->resourceResponse($data, 'home.home_success');
+
+    }
+}
